4315d3b0c31406f919316cd3fdb6c5eac0a7cacc
Zwartkat <Zwartkat@users.noreply.github.com>
2026-03-08 21:42:05 +0000

    deploy: 1c766c5169bfd29f3417b7196357e5109974c2ed

--- api/Pong/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ClavierBorneArcade"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"Pong"}];updateSearchResults();
